03ce4a838edd70fec07b02eed4803d541790ac85
kruthi-msd <106132887+kruthi-msd@users.noreply.github.com>
2024-03-18 15:48:10 +0530

    Update Serialization.java

--- Serialization.java
+++ Serialization.java
@@ -2,63 +2,34 @@
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
-class Customer implements Serializable {
-    private static final long serialVersionUID = 1L;
+
+class Customer implements java.io.Serializable {
     private int id;
     private String name;
     private String contactNo;
     private String address;
+
     public Customer(int id, String name, String contactNo, String address) {
         this.id = id;
         this.name = name;
         this.contactNo = contactNo;
         this.address = address;
     }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getContactNo() {
-        return contactNo;
-    }
-    public void setContactNo(String contactNo) {
-        this.contactNo = contactNo;
-    }
-    public String getAddress() {
-        return address;
-    }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    // toString method to display object information
-    @Override
-    public String toString() {
-        return "Customer [id=" + id + ", name=" + name + ", contactNo=" + contactNo + ", address=" + address + "]";
-    }
+    
 }
 
-public class SerializationExample {
+public class Main{
     public static void main(String[] args) {
-        Customer customer = new Customer(101, "John Doe", "1234567890", "123 Main St, City");
+        Customer customer = new Customer(101, "Alice", "1234567890", "New York");
+
         try {
-            // Create ObjectOutputStream to write objects to a file
             FileOutputStream fileOut = new FileOutputStream("JavaObject.txt");
-            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-            objectOut.writeObject(customer);
-            objectOut.close();
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(customer);
+            out.close();
             fileOut.close();
-            System.out.println("Customer object serialized successfully.");
+            System.out.println("Customer object serialized to JavaObject.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
